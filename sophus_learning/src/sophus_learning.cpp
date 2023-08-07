e51b6080c4cb6c1cad12ee73a1190396a51f9276
llxClover <987197949@qq.com>
2023-08-07 21:20:21 +0800

    finished sophus learning

--- sophus_learning/src/sophus_learning.cpp
+++ sophus_learning/src/sophus_learning.cpp
@@ -32,8 +32,39 @@ int main(int argc, char const *argv[]) {
   Eigen::Vector3d so3_hat_vee = Sophus::SO3d::vee(so3_hat);
   std::cout << "hat matrix to vector by vee:\n"
             << so3_hat_vee.transpose() << std::endl;
-  
-  
+
+  // 增量扰动模型(李群)
+  std::cout << "-------增量扰动模型SO3-------" << std::endl;
+  Eigen::Vector3d update_so3(1e-4, 0, 0); // 增量
+  Sophus::SO3d SO3_updated = Sophus::SO3d::exp(update_so3) * SO3_R;
+  std::cout << "SO3 updated= \n" << SO3_updated.matrix() << std::endl;
+
+  std::cout << "**************************" << std::endl;
+  Eigen::Vector3d t(1, 0, 0);
+  Sophus::SE3d SE3_Rt(R, t);
+  Sophus::SE3d SE3_qt(q, t);
+  std::cout << "SE3 structed from (R,t) : \n" << SE3_Rt.matrix() << std::endl;
+  std::cout << "SE3 structed from (q,t) : \n" << SE3_qt.matrix() << std::endl;
+  // 李代数se(3)是6维向量
+  Eigen::VectorXd se3 = SE3_Rt.log();
+  std::cout << "se3= " << se3.transpose() << std::endl;
+
+  typedef Eigen::Matrix<double, 6, 1> Vector6d;
+  Vector6d se3_1 = SE3_qt.log();
+  std::cout << "se3_1= " << se3_1.transpose() << std::endl;
+
+  std::cout << "se3 hat = \n" << Sophus::SE3d::hat(se3_1).matrix() << std::endl;
+  std::cout << "se3 vee = \n"
+            << Sophus::SE3d::vee(Sophus::SE3d::hat(se3_1).matrix()).transpose()
+            << std::endl;
+
+  std::cout << "-------增量扰动模型SE3-------" << std::endl;
+  Vector6d update_se3;
+  // update_se3 << 1e-4, 0, 0, 0, 0, 0;
+  update_se3.setZero();
+  update_se3(0, 0) = 1e-4;
+  Sophus::SE3d SE3_updated = Sophus::SE3d::exp(update_se3) * SE3_Rt;
+  std::cout << "Se3 updated : \n" << SE3_updated.matrix() << std::endl;
 
   return 0;
 }
