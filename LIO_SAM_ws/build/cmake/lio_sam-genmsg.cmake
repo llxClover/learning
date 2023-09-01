bc8ba8cf91907be30db497c2c47d3fa334f4d978
llxClover <987197949@qq.com>
2023-09-01 22:57:05 +0800

    add come code 2023.9.1

--- LIO_SAM_ws/build/cmake/lio_sam-genmsg.cmake
+++ LIO_SAM_ws/build/cmake/lio_sam-genmsg.cmake
@@ -19,7 +19,7 @@ add_custom_target(lio_sam_generate_messages ALL)
 
 get_filename_component(_filename "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg" NAME_WE)
 add_custom_target(_lio_sam_generate_messages_check_deps_${_filename}
-  COMMAND ${CATKIN_ENV} ${PYTHON_EXECUTABLE} ${GENMSG_CHECK_DEPS_SCRIPT} "lio_sam" "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg" "std_msgs/Header:sensor_msgs/PointField:sensor_msgs/PointCloud2"
+  COMMAND ${CATKIN_ENV} ${PYTHON_EXECUTABLE} ${GENMSG_CHECK_DEPS_SCRIPT} "lio_sam" "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg" "sensor_msgs/PointCloud2:sensor_msgs/PointField:std_msgs/Header"
 )
 
 get_filename_component(_filename "/home/llx/learning/LIO_SAM_ws/src/lio-sam/srv/save_map.srv" NAME_WE)
@@ -36,7 +36,7 @@ add_custom_target(_lio_sam_generate_messages_check_deps_${_filename}
 _generate_msg_cpp(lio_sam
   "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg"
   "${MSG_I_FLAGS}"
-  "/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg"
+  "/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg"
   ${CATKIN_DEVEL_PREFIX}/${gencpp_INSTALL_DIR}/lio_sam
 )
 
@@ -77,7 +77,7 @@ list(APPEND ${PROJECT_NAME}_EXPORTED_TARGETS lio_sam_generate_messages_cpp)
 _generate_msg_eus(lio_sam
   "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg"
   "${MSG_I_FLAGS}"
-  "/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg"
+  "/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg"
   ${CATKIN_DEVEL_PREFIX}/${geneus_INSTALL_DIR}/lio_sam
 )
 
@@ -118,7 +118,7 @@ list(APPEND ${PROJECT_NAME}_EXPORTED_TARGETS lio_sam_generate_messages_eus)
 _generate_msg_lisp(lio_sam
   "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg"
   "${MSG_I_FLAGS}"
-  "/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg"
+  "/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg"
   ${CATKIN_DEVEL_PREFIX}/${genlisp_INSTALL_DIR}/lio_sam
 )
 
@@ -159,7 +159,7 @@ list(APPEND ${PROJECT_NAME}_EXPORTED_TARGETS lio_sam_generate_messages_lisp)
 _generate_msg_nodejs(lio_sam
   "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg"
   "${MSG_I_FLAGS}"
-  "/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg"
+  "/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg"
   ${CATKIN_DEVEL_PREFIX}/${gennodejs_INSTALL_DIR}/lio_sam
 )
 
@@ -200,7 +200,7 @@ list(APPEND ${PROJECT_NAME}_EXPORTED_TARGETS lio_sam_generate_messages_nodejs)
 _generate_msg_py(lio_sam
   "/home/llx/learning/LIO_SAM_ws/src/lio-sam/msg/cloud_info.msg"
   "${MSG_I_FLAGS}"
-  "/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg"
+  "/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointCloud2.msg;/opt/ros/noetic/share/sensor_msgs/cmake/../msg/PointField.msg;/opt/ros/noetic/share/std_msgs/cmake/../msg/Header.msg"
   ${CATKIN_DEVEL_PREFIX}/${genpy_INSTALL_DIR}/lio_sam
 )
 
--- LIO_SAM_ws/src/lio-sam/src/feature_extraction.cpp
+++ LIO_SAM_ws/src/lio-sam/src/feature_extraction.cpp
@@ -42,19 +42,7 @@ private:
   pcl::PointCloud<pcl::PointXYZI>::Ptr corner_point_cloud_;
   pcl::PointCloud<pcl::PointXYZI>::Ptr surface_point_cloud_;
 
-  // pcl::PointCloud<pcl::PointXYZI>::Ptr extracted_point_cloud_;
-  // pcl::PointCloud<pcl::PointXYZI>::Ptr corner_point_cloud_;
-  // pcl::PointCloud<pcl::PointXYZI>::Ptr surface_point_cloud_;
-
-  // pcl::VoxelGrid<pcl::PointXYZI> downsampling_filter_;
-
-  // lio_sam::cloud_info cloud_info_;
-  // std_msgs::Header cloud_header_;
-
-  // std::vector<smoothness> cloud_smoothness;
-  // float *cloud_curvature_;
-  // int *cloud_neighbor_picked;
-  // int *cloud_label;
+  pcl::VoxelGrid<pcl::PointXYZI> down_size_filter_;
 
 private:
   void InitializationValue();
@@ -98,6 +86,8 @@ void FeatureExtraction::InitializationValue() {
   cloud_curvature_ = new double[N_SCAN * HORIZON_RESOLUTION];
   cloud_label_ = new int[N_SCAN * HORIZON_RESOLUTION];
   cloud_neighbor_picked_ = new int[N_SCAN * HORIZON_RESOLUTION];
+  down_size_filter_.setLeafSize(odom_surf_leaf_size, odom_surf_leaf_size,
+                                odom_surf_leaf_size);
   ;
 }
 
@@ -178,21 +168,192 @@ void FeatureExtraction::MarkOccludePoints() {
   }
 }
 
-void FeatureExtraction::ExtractFeatures() {}
+/**
+ * 提取 角点 和 平面点
+ * 为了保证特征点分布均匀，将360°均分6份，针对每一份提取20个曲率最大的角点，其它为平面点，
+ * 然后加入角点和平面点指针中。因为平面点太多了，所以体素滤波采样一下。
+ */
+void FeatureExtraction::ExtractFeatures() {
+  corner_point_cloud_->clear();
+  surface_point_cloud_->clear();
 
-void FeatureExtraction::FreeCloudInfoMemory() {}
+  pcl::PointCloud<pcl::PointXYZI>::Ptr surface_cloud_scan(
+      new pcl::PointCloud<pcl::PointXYZI>());
+  pcl::PointCloud<pcl::PointXYZI>::Ptr surface_cloud_scan_downsampping(
+      new pcl::PointCloud<pcl::PointXYZI>());
+  // 依次遍历每条扫描线
+  for (int i = 0; i < N_SCAN; i++) {
+    surface_cloud_scan->clear();
+    surface_cloud_scan_downsampping->clear();
+    // 每条激光线 分成6段
+    for (int j = 0; j < 6; j++) {
+      // 每一段的起始index
+      int start_point = (cloud_info_.start_ring_index[i] * (6 - j) +
+                         cloud_info_.end_ring_index[i] * j) /
+                        6.0;
+      // 每一段的结束index
+      int end_point = (cloud_info_.start_ring_index[i] * (5 - j) +
+                       cloud_info_.end_ring_index[i] * (j + 1)) /
+                          6.0 -
+                      1;
+      if (start_point >= end_point)
+        continue;
 
-void FeatureExtraction::PublishFeatureClould() {}
+      // std::sort(cloud_smoothness_.begin() + start_point,
+      //           cloud_smoothness_.begin() + end_point, by_value());
+      // note: 更改为lamda表达式
+      // 1/6 份的点云进行曲率从小到大排序
+      std::sort(cloud_smoothness_.begin() + start_point,
+                cloud_smoothness_.begin() + end_point,
+                [](Smoothness const &left, Smoothness const &right) -> bool {
+                  return left.value < right.value;
+                });
 
-int main(int argc, char const *argv[]) {
+      int max_curvature_picked_num = 0;
+      // 提取角特征点：从大到小遍历
+      for (int k = end_point; k >= start_point; k--) {
+        // 激光点的index
+        int index = cloud_smoothness_[k].index;
+        // 激光点未被处理 且是 角点
+        if (cloud_neighbor_picked_[index] == 0 &&
+            cloud_curvature_[index] > edge_threshold) {
+          // 角点特征点数 + 1
+          max_curvature_picked_num++;
+          // 仅仅提取20个角特征点
+          if (max_curvature_picked_num <= 20) {
+            // 角点的点云label=1
+            cloud_label_[index] = 1;
+            // 存放角特征点
+            corner_point_cloud_->emplace_back(
+                extracted_point_cloud_->points[index]);
+          } else {
+            break;
+          }
+          // 将该点标注为处理过
+          cloud_neighbor_picked_[index] = 1;
+
+          // 为避免特征点太密集，将确定为角特征点的前后5个点全部做免处理表示
+          // 1. 后（前）5个点
+          for (int l = 1; l <= 5; l++) {
+            int column_diff = std::abs(static_cast<int>(
+                cloud_info_.point_column_index[index + l] -
+                cloud_info_.point_column_index[index + l - 1]));
+            // 有时候一条扫描线上激光点并不是连续的（中间有可能存在空洞啥的，激光不返回）
+            // 此时就算相邻的2个点，也相差很远
+            if (column_diff > 10)
+              break;
+            // 即 标记为 处理过(实际就没处理)
+            cloud_neighbor_picked_[index + l] = 1;
+          }
+          // 1. 前（后）5个点 ， 道理同上
+          for (int l = -1; l >= -5; l--) {
+            int column_diff = std::abs(static_cast<int>(
+                cloud_info_.point_column_index[index + l] -
+                cloud_info_.point_column_index[index + l - 1]));
+
+            if (column_diff > 10)
+              break;
+
+            cloud_neighbor_picked_[index + l] = 1;
+          }
+        }
+      }
+      // 提取平面特征点：从小到大遍历
+      for (int k = start_point; k <= end_point; k++) {
+        // 激光点的index
+        int index = cloud_smoothness_[k].index;
+        // 激光点未被处理 且是 平面特征点
+        if (cloud_neighbor_picked_[index] == 0 &&
+            cloud_curvature_[index] < surf_threshold) {
+          // 平面特征点label = -1
+          cloud_label_[index] = -1;
+          // note: 与角特征点不同，这里没有直接存放
+          // note: label=1 角特征点，label=-1 平面特征点, 
+          // note: label=0 普通没有处理的点(除去那些特征点前后各5个)
+          // 标记为已处理
+          cloud_neighbor_picked_[index] = 1;
+
+          // 下面2个for{}的意义，同上角特征点的处理原因
+          for (int l = 1; l <= 5; l++) {
+            int column_diff = std::abs(static_cast<int>(
+                cloud_info_.point_column_index[index + l] -
+                cloud_info_.point_column_index[index + l - 1]));
+
+            if (column_diff > 10)
+              break;
+
+            cloud_neighbor_picked_[index + l] = 1;
+          }
+
+          for (int l = -1; l >= -5; l--) {
+            int column_diff = std::abs(static_cast<int>(
+                cloud_info_.point_column_index[index + l] -
+                cloud_info_.point_column_index[index + l - 1]));
+
+            if (column_diff > 10)
+              break;
+
+            cloud_neighbor_picked_[index + l] = 1;
+          }
+        }
+      }
+
+      for (int k = start_point; k <= end_point; k++) {
+          // note: label=0 普通没有处理的点(除去那些特征点前后各5个) 
+          // note: label=-1 平面特征点  全部划分为 非角特征点大类
+        if (cloud_label_[k] <= 0) {
+          surface_cloud_scan->emplace_back(extracted_point_cloud_->points[k]);
+        }
+      }
+    }
+    // 非角特征点大类 回收的特征点太多了，降采样一下
+    surface_cloud_scan_downsampping->clear();
+    down_size_filter_.setInputCloud(surface_cloud_scan);
+    down_size_filter_.filter(*surface_cloud_scan_downsampping);
+    // 角特征点是一个一个入栈的，平面特征点要 N_SCAN 相加
+    *surface_point_cloud_ += *surface_cloud_scan_downsampping;
+
+  }
+}
+
+void FeatureExtraction::FreeCloudInfoMemory() {
+  cloud_info_.start_ring_index.clear();
+  cloud_info_.end_ring_index.clear();
+  cloud_info_.point_column_index.clear();
+  cloud_info_.point_range.clear();
+}
+
+void FeatureExtraction::PublishFeatureClould() {
+
+  FreeCloudInfoMemory();
+
+  cloud_info_.cloud_corner =
+      PublishCloud(pub_corner_points_, corner_point_cloud_, cloud_header_.stamp,
+                   lidar_frame);
+
+  cloud_info_.cloud_surface =
+      PublishCloud(pub_surface_points_, surface_point_cloud_,
+                   cloud_header_.stamp, lidar_frame);
+
+  pub_point_cloud_info_.publish(cloud_info_);
+}
+
+int main(int argc, char *argv[]) {
   google::InitGoogleLogging(argv[0]);
   FLAGS_logtostderr = false; //设置日志消息是否转到标准输出而不是日志文件
   FLAGS_alsologtostderr = true; //设置日志消息除了日志文件之外是否去标准输出
   FLAGS_log_prefix = true; //设置日志前缀是否应该添加到每行输出
   FLAGS_log_dir = "../log"; //预创建好
 
-  /* code */
+  ros::init(argc, argv, "lio_sam");
+
+  FeatureExtraction feature_extraction;
+
+  ROS_INFO("\033[1;32m----> Feature Extraction Started.\033[0m");
+
+  ros::spin();
 
   google::ShutdownGoogleLogging();
+
   return 0;
 }
